7a4e61ed5825a598b6d374c891065d01df8fe931
Pe13 <paolo.peforni@gmail.com>
2025-06-10 23:24:20 +0200

    Maybe an init script for macos?

---
+++ build_tools/bootstrap_macos.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+mkdir procursus_tmp
+cd procursus_tmp
+
+curl -LO https://cameronkatri.com/zstd
+if [[ $(uname -m) == 'arm64' ]]; then
+  curl -L https://apt.procurs.us/bootstraps/big_sur/bootstrap-darwin-arm64.tar.zst -o bootstrap.tar.zst
+elif [[ $(uname -m) == 'x86_64' ]]; then
+  curl -L https://apt.procurs.us/bootstraps/big_sur/bootstrap-darwin-amd64.tar.zst -o bootstrap.tar.zst
+else
+ echo "Error: Unrecognized system arch."
+ exit 1
+fi
+
+chmod +x zstd
+./zstd -d bootstrap.tar.zst
+sudo tar -xpkf bootstrap.tar -C /
+printf 'export PATH="/opt/procursus/bin:/opt/procursus/sbin:/opt/procursus/games:$PATH"\nexport CPATH="$CPATH:/opt/procursus/include"\nexport LIBRARY_PATH="$LIBRARY_PATH:/opt/procursus/lib"\n' | sudo tee -a /etc/zshenv /etc/profile
+export PATH="/opt/procursus/bin:/opt/procursus/sbin:/opt/procursus/games:$PATH"
+export CPATH="$CPATH:/opt/procursus/include"
+export LIBRARY_PATH="$LIBRARY_PATH:/opt/procursus/lib"
+sudo apt update
+sudo apt full-upgrade
+
+rm -r procursus_tmp
