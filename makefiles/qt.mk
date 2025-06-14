5950bf5732e2fc34ef36ced98962f01ed30430c7
Pe13 <paolo.peforni@gmail.com>
2025-06-14 06:09:33 -0700

    Qt now builds

--- makefiles/qt.mk
+++ makefiles/qt.mk
@@ -6,38 +6,51 @@ SUBPROJECTS   += qt
 QT_VERSION    := 6.5.3
 DEB_QT_V      ?= $(QT_VERSION)
 
-qt-setup: setup
+
+qt-setup: setup libpng16-setup
 	$(call GITHUB_ARCHIVE,qt,qtbase,$(QT_VERSION),$(QT_VERSION))
 	$(call EXTRACT_TAR,qtbase-$(QT_VERSION).tar.gz,qtbase-$(QT_VERSION),qt)
+	
+	# Bundle an updated libpng too since I can't let qt find the system one :'(
+	$(BUILD_WORK)/qt/src/3rdparty/libpng/import_from_libpng_tarball.sh \
+		$(BUILD_WORK)/libpng16 \
+		$(BUILD_WORK)/qt/src/3rdparty/libpng
+	
 	mkdir -p $(BUILD_WORK)/qt/build-macos
 	mkdir -p $(BUILD_WORK)/qt/build-ios
+
+ifneq ($(wildcard $(BUILD_WORK)/qt/.macos_build_complete),)
+qt-macos:
+	@echo "Using previously built macos qt."
+else
+qt-macos: qt-setup
+	# Build for macos first, since this version won't probably match the system one
+	cd $(BUILD_WORK)/qt/build-macos && \
+		unset CFLAGS CXXFLAGS ASFLAGS CXXFLAGS CPPFLAGS LDFLAGS && \
+		../configure \
+		-prefix $(BUILD_WORK)/qt/macos-qt \
+		-release \
+		-system-zlib \
+		-qt-libjpeg \
+		-no-libpng \
+		-qt-freetype \
+		-qt-pcre \
+		-qt-harfbuzz
+	cmake --build $(BUILD_WORK)/qt/build-macos --parallel
+	cmake --install $(BUILD_WORK)/qt/build-macos
+	touch $(BUILD_WORK)/qt/.macos_build_complete
+endif
 
 ifneq ($(wildcard $(BUILD_WORK)/qt/.build_complete),)
 qt:
 	@echo "Using previously built qt."
 else
-qt: qt-setup
-	# Build for macos first, since this version won't probably match the system one
-	cd $(BUILD_WORK)/qt/build-macos && ../configure \
-		-prefix $(BUILD_WORK)/qt/macos-qt \
+qt: qt-setup qt-macos libpng16
+	cd $(BUILD_WORK)/qt/build-ios && \
+		unset CFLAGS CXXFLAGS ASFLAGS CXXFLAGS CPPFLAGS LDFLAGS && \
+		../configure \
+		-platform macx-ios-clang \
 		-release \
-		CC="$(CC_FOR_BUILD)" \
-		CXX="$(CXX_FOR_BUILD)" \
-		CPP="$(CPP_FOR_BUILD)" \
-		AR="$(AR_FOR_BUILD)" \
-		RANLIB="$(RANLIB_FOR_BUILD)" \
-		STRIP="$(STRIP_FOR_BUILD)" \
-		CFLAGS="$(CFLAGS_FOR_BUILD)" \
-		CXXFLAGS="$(CXXFLAGS_FOR_BUILD)" \
-		CPPFLAGS="$(CPPFLAGS_FOR_BUILD)" \
-		ASFLAGS="$(ASFLAGS_FOR_BUILD)" \
-		LDFLAGS="$(LDFLAGS_FOR_BUILD)"
-	cmake --build $(BUILD_WORK)/qt/build-macos --parallel
-	cmake --install $(BUILD_WORK)/qt/build-macos
-
-	# Finally built Qt for iOS
-	cd $(BUILD_WORK)/qt/build-ios && ../configure \
-		-platform macx-ios-clang -release \
 		-qt-host-path $(BUILD_WORK)/qt/macos-qt \
 		-sdk iphoneos \
 		-prefix $(BUILD_STAGE)/qt
