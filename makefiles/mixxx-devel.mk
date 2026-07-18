2c9460572c436b2f84b7076efea4c06664463d30
Pe13 <paolo.peforni@gmail.com>
2026-07-18 18:47:15 +0200

    Clean mixxx.mk and create a mixxx-devel.mk for development

---
+++ makefiles/mixxx-devel.mk
@@ -0,0 +1,88 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS   += mixxx
+MIXXX_VERSION := 2.6.0
+DEB_MIXXX_V   ?= $(MIXXX_VERSION)
+
+MIXXX_DEPS = chromaprint ffmpeg flac fftw googletest hidapi libdjinterop libebur128 libid3tag libkeyfinder libmad libmodplug libogg libopus libprotobuf libsndfile libtheora libvorbis lilv microsoft-gsl mpg123 opusfile portaudio portmidi qt rubberband soundtouch taglib wavpack
+
+mixxx-devel-setup: setup
+	$(call GIT_CLONE,https://github.com/Pe13/mixxx.git,ios,mixxx-devel)
+
+mixxx-configure: mixxx-devel-setup $(MIXXX_DEPS)
+ifneq ($(UNAME),Linux)
+	# WARNING: this action should be undone if you need to build a package that is not Mixxx
+	if [ ! -d $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/c++ ]; then \
+		mkdir -p $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/c++/v1; \
+		cp -arf /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS17.2.sdk/usr/include/c++/v1/stdlib.h \
+			$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/c++/v1; \
+		fi
+endif
+	rm -rf $(BUILD_WORK)/mixxx-devel/cmake-build-relwithdebinfo
+	cmake -S $(BUILD_WORK)/mixxx-devel -B $(BUILD_WORK)/mixxx-devel/cmake-build-relwithdebinfo \
+		$(DEFAULT_CMAKE_FLAGS) \
+		-DRIGHT_PROTOC="$(BUILD_WORK)/libprotobuf/build-host/protoc" \
+		-DCMAKE_PREFIX_PATH="$(BUILD_BASE)/usr/;$(TARGET_SYSROOT)/usr" \
+		-DCMAKE_FRAMEWORK_PATH="$(TARGET_SYSROOT)/Developer/Library/Frameworks;$(TARGET_SYSROOT)/System/Library/Frameworks" \
+		-DCMAKE_FIND_USE_CMAKE_PATH=ON \
+		-DCMAKE_FIND_USE_CMAKE_ENVIRONMENT_PATH=OFF \
+		-DCMAKE_FIND_USE_SYSTEM_ENVIRONMENT_PATH=ON \
+		-DCMAKE_FIND_USE_CMAKE_SYSTEM_PATH=OFF \
+		-DCMAKE_FIND_USE_INSTALL_PREFIX=ON \
+		-DCMAKE_MAKE_PROGRAM="/usr/bin/ninja" \
+		-DCMAKE_OSX_DEPLOYMENT_TARGET=14.0 \
+		-G"Ninja" \
+		-DIOS=ON \
+		-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+		-DQT6=ON \
+		-DOPUS=ON \
+		-DMAD=ON \
+		-DMODPLUG=ON \
+		-DQTKEYCHAIN=OFF \
+		-DBATTERY=OFF \
+		-DBUILD_BENCH=OFF \
+		-DBUILD_TESTING=OFF \
+		-DQT_NO_SET_DEFAULT_IOS_LAUNCH_SCREEN=ON \
+		-DQT_NO_ADD_IOS_LAUNCH_SCREEN_TO_BUNDLE=ON
+
+
+mixxx-build: mixxx-devel-setup
+	cmake --build $(BUILD_WORK)/mixxx-devel/cmake-build-relwithdebinfo --target mixxx --config RelWithDebInfo
+
+	$(call AFTER_BUILD)
+
+
+mixxx-package:
+	# Cleanup previous build artifacts if any
+	rm -rf $(BUILD_WORK)/mixxx-devel/Mixxx.tipa $(BUILD_WORK)/mixxx-devel/Payload
+
+	# Generate dSYM
+	dsymutil $(BUILD_WORK)/mixxx-devel/cmake-build-relwithdebinfo/Mixxx.app/Mixxx \
+		-o $(BUILD_WORK)/mixxx-devel/Mixxx.dSYM
+
+	# Add entitlements to the executable
+	$(LDID) -S$(BUILD_WORK)/mixxx-devel/packaging/ios/Mixxx-devel.entitlements \
+		$(BUILD_WORK)/mixxx-devel/cmake-build-relwithdebinfo/Mixxx.app/Mixxx
+
+	# Tipa time
+	mkdir $(BUILD_WORK)/mixxx-devel/Payload
+	cp -a $(BUILD_WORK)/mixxx-devel/cmake-build-relwithdebinfo/Mixxx.app \
+		$(BUILD_WORK)/mixxx-devel/Payload
+	cp -a $(BUILD_WORK)/mixxx-devel/packaging/ios/Assets.xcassets/AppIcon.appiconset/1024x1024.png \
+		$(BUILD_WORK)/mixxx-devel/Payload/Mixxx.app/Icon.png
+	cd $(BUILD_WORK)/mixxx-devel/ && \
+		zip -r Mixxx.tipa Payload
+
+mixxx-install: mixxx-package
+ifeq ($(DEVICE_IP),)
+	@echo "DEVICE_IP is not set. Please set it to the IP address of your iOS device in the make invokation."
+	@exit 1
+else
+	# You need to run "nohup python3 -m http.server 8000 --directory /var/mobile" once on your iOS device before running this command.
+	scp $(BUILD_WORK)/mixxx-devel/Mixxx.tipa mobile@$(DEVICE_IP):/var/mobile/Mixxx.tipa
+	ssh mobile@$(DEVICE_IP) 'uiopen "apple-magnifier://install?url=http://127.0.0.1:8000/Mixxx.tipa"'
+endif
+
+.PHONY: mixxx mixxx-package mixxx-configure mixxx-build mixxx-install
--- makefiles/mixxx.mk
+++ makefiles/mixxx.mk
@@ -3,24 +3,26 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS   += mixxx
-MIXXX_VERSION := 2.5.0
+MIXXX_VERSION := 2.6.0
 DEB_MIXXX_V   ?= $(MIXXX_VERSION)
-
-MIXXX_DEPS_TO_BUILD = chromaprint ffmpeg flac fftw googletest hidapi libdjinterop libebur128 libid3tag libkeyfinder libmad libmodplug libogg libopus libprotobuf libsndfile libtheora libvorbis lilv microsoft-gsl mpg123 opusfile portaudio portmidi qt rubberband soundtouch taglib wavpack
-MIXXX_DEPS_TO_DOWNLOAD =
-MIXXX_DEPS = $(MIXXX_DEPS_TO_BUILD) $(MIXXX_DEPS_TO_DOWNLOAD)
-MIXXX_ALL_DEPS_TO_DOWNLOAD = $(shell $(BUILD_TOOLS)/calc_packages_deps.py $(MIXXX_DEPS_TO_DOWNLOAD))
 
 mixxx-setup: setup
 	$(call GIT_CLONE,https://github.com/Pe13/mixxx.git,ios,mixxx)
 
+ifneq ($(wildcard $(BUILD_WORK)/mixxx/.build_complete),)
+mixxx-build:
+	@echo "Using previously built mixxx."
+else
 mixxx: mixxx-configure mixxx-build mixxx-package
+	@echo
+	@echo
+	@echo "--------------------------------"
+	@echo
+	@echo "Mixxx.tipa is located at: $(BUILD_WORK)/mixxx/Mixxx.tipa"
+	$(call AFTER_BUILD)
+endif
 
-
-mixxx-remove-shared: $(MIXXX_DEPS)
-	rm $(BUILD_BASE)/*/.dylib
-
-mixxx-configure: mixxx-setup $(MIXXX_DEPS)
+mixxx-configure: mixxx-setup chromaprint ffmpeg flac fftw googletest hidapi libdjinterop libebur128 libid3tag libkeyfinder libmad libmodplug libogg libopus libprotobuf libsndfile libtheora libvorbis lilv microsoft-gsl mpg123 opusfile portaudio portmidi qt rubberband soundtouch taglib wavpack
 ifneq ($(UNAME),Linux)
 	if [ ! -d $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/c++ ]; then \
 		mkdir -p $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/c++/v1; \
@@ -28,8 +30,8 @@ ifneq ($(UNAME),Linux)
 		$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/c++/v1; \
 		fi
 endif
-	rm -rf $(BUILD_WORK)/mixxx/cmake-build-relwithdebinfo
-	cmake -S $(BUILD_WORK)/mixxx -B $(BUILD_WORK)/mixxx/cmake-build-relwithdebinfo \
+	rm -rf $(BUILD_WORK)/mixxx/build
+	cmake -S $(BUILD_WORK)/mixxx -B $(BUILD_WORK)/mixxx/build \
 		$(DEFAULT_CMAKE_FLAGS) \
 		-DRIGHT_PROTOC="$(BUILD_WORK)/libprotobuf/build-host/protoc" \
 		-DCMAKE_PREFIX_PATH="$(BUILD_BASE)/usr/;$(TARGET_SYSROOT)/usr" \
@@ -43,7 +45,6 @@ endif
 		-DCMAKE_OSX_DEPLOYMENT_TARGET=14.0 \
 		-G"Ninja" \
 		-DIOS=ON \
-		-DCMAKE_BUILD_TYPE=RelWithDebInfo \
 		-DQT6=ON \
 		-DOPUS=ON \
 		-DMAD=ON \
@@ -55,36 +56,25 @@ endif
 		-DQT_NO_SET_DEFAULT_IOS_LAUNCH_SCREEN=ON \
 		-DQT_NO_ADD_IOS_LAUNCH_SCREEN_TO_BUNDLE=ON
 
-ifneq ($(wildcard $(BUILD_WORK)/mixxx/.build_complete),)
-mixxx-build:
-	@echo "Using previously built mixxx."
-else
-mixxx-build: mixxx-setup $(MIXXX_DEPS)
-	cmake --build $(BUILD_WORK)/mixxx/cmake-build-relwithdebinfo --target mixxx --config RelWithDebInfo
-
-	$(call AFTER_BUILD)
-endif
+mixxx-build: mixxx-configure
+	cmake --build $(BUILD_WORK)/mixxx/build --target mixxx
 
 
-mixxx-package:
+mixxx-package: mixxx-build
 	# Cleanup previous build artifacts if any
 	rm -rf $(BUILD_WORK)/mixxx/Mixxx.tipa $(BUILD_WORK)/mixxx/Payload
 
-	# Generate dSYM
-	dsymutil $(BUILD_WORK)/mixxx/cmake-build-relwithdebinfo/Mixxx.app/Mixxx \
-		-o $(BUILD_WORK)/mixxx/Mixxx.dSYM
-
 	# Add entitlements to the executable
-	$(LDID) -S$(BUILD_WORK)/mixxx/packaging/macos/Mixxx.entitlements \
-		$(BUILD_WORK)/mixxx/cmake-build-relwithdebinfo/Mixxx.app/Mixxx
+	$(LDID) -S$(BUILD_WORK)/mixxx/packaging/ios/Mixxx.entitlements \
+		$(BUILD_WORK)/mixxx/build/Mixxx.app/Mixxx
 
 	# Tipa time
 	mkdir $(BUILD_WORK)/mixxx/Payload
-	cp -a $(BUILD_WORK)/mixxx/cmake-build-relwithdebinfo/Mixxx.app \
+	cp -a $(BUILD_WORK)/mixxx/build/Mixxx.app \
 		$(BUILD_WORK)/mixxx/Payload
 	cp -a $(BUILD_WORK)/mixxx/packaging/ios/Assets.xcassets/AppIcon.appiconset/1024x1024.png \
 		$(BUILD_WORK)/mixxx/Payload/Mixxx.app/Icon.png
 	cd $(BUILD_WORK)/mixxx/ && \
 		zip -r Mixxx.tipa Payload
 
-.PHONY: mixxx mixxx-package
+.PHONY: mixxx mixxx-package mixxx-configure mixxx-build mixxx-install
